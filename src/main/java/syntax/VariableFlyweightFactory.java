26908d3295f978fd75fd7ce18ad7a57563cc5548
Nyveon <54124297+Nyveon@users.noreply.github.com>
2021-07-05 00:25:06 -0400

    FlyweightFactory

---
+++ src/main/java/syntax/VariableFlyweightFactory.java
@@ -0,0 +1,104 @@
+package syntax;
+
+import types.IScrabbleVariable;
+import types.ScrabbleBool;
+import types.ScrabbleString;
+import types.numbers.ScrabbleBinary;
+import types.numbers.ScrabbleFloat;
+import types.numbers.ScrabbleInt;
+
+import java.util.HashMap;
+
+/**
+ * Flyweight object for optimizing memory usage of ScrabbleVariables
+ */
+public class VariableFlyweightFactory {
+    static HashMap<String, IScrabbleVariable> variables = new HashMap<String, IScrabbleVariable>();
+
+    // nota a ayudante: no alcancé a hacer esto para los nodes T_T
+
+    /**
+     * Getter. Returns variable with given name. Null if variable does not exist.
+     * @param name String name of variable
+     * @return null if not in hashmap. ScrabbleVariable object if it exists.
+     */
+    static IScrabbleVariable getVariable(String name) {
+        return variables.get(name);
+    }
+
+    /**
+     * Flyweight factory for ScrabbleString
+     * @param name Name of the variable
+     * @param value Value of the variable
+     * @return Resulting variable (New if not previously made, pre-existing if exists)
+     */
+    static ScrabbleString createScrabbleString(String name, String value) {
+        ScrabbleString result = (ScrabbleString) variables.get(name);
+        if (result == null) {
+            result = new ScrabbleString(value);
+            variables.put(name, result);
+        }
+        return result;
+    }
+
+    /**
+     * Flyweight factory for ScrabbleBool
+     * @param name Name of the variable
+     * @param value Value of the variable
+     * @return Resulting variable (New if not previously made, pre-existing if exists)
+     */
+    static ScrabbleBool createScrabbleBool(String name, boolean value) {
+        ScrabbleBool result = (ScrabbleBool) variables.get(name);
+        if (result == null) {
+            result = new ScrabbleBool(value);
+            variables.put(name, result);
+        }
+        return result;
+    }
+
+    /**
+     * Flyweight factory for ScrabbleFloat
+     * @param name Name of the variable
+     * @param value Value of the variable
+     * @return Resulting variable (New if not previously made, pre-existing if exists)
+     */
+    static ScrabbleFloat createScrabbleFloat(String name, double value) {
+        ScrabbleFloat result = (ScrabbleFloat) variables.get(name);
+        if (result == null) {
+            result = new ScrabbleFloat(value);
+            variables.put(name, result);
+        }
+        return result;
+    }
+
+    /**
+     * Flyweight factory for ScrabbleInt
+     * @param name Name of the variable
+     * @param value Value of the variable
+     * @return Resulting variable (New if not previously made, pre-existing if exists)
+     */
+    static ScrabbleInt createScrabbleInt(String name, int value) {
+        ScrabbleInt result = (ScrabbleInt) variables.get(name);
+        if (result == null) {
+            result = new ScrabbleInt(value);
+            variables.put(name, result);
+        }
+        return result;
+    }
+
+    /**
+     * Flyweight factory for ScrabbleBinary
+     * @param name Name of the variable
+     * @param value Value of the variable
+     * @return Resulting variable (New if not previously made, pre-existing if exists)
+     */
+    static ScrabbleBinary createScrabbleBinary(String name, String value) {
+        ScrabbleBinary result = (ScrabbleBinary) variables.get(name);
+        if (result == null) {
+            result = new ScrabbleBinary(value);
+            variables.put(name, result);
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/syntax/VariableFlyweightFactoryTest.java
@@ -0,0 +1,80 @@
+package syntax;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import syntax.endnodes.*;
+import types.IScrabbleVariable;
+import types.ScrabbleBool;
+import types.ScrabbleString;
+import types.numbers.ScrabbleBinary;
+import types.numbers.ScrabbleFloat;
+import types.numbers.ScrabbleInt;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VariableFlyweightFactoryTest {
+    protected int seed;
+    protected Random rng;
+
+    @BeforeEach
+    void setUp() {
+        seed = new Random().nextInt();
+        rng = new Random(seed);
+    }
+
+    // Cant be repeated because of the hashmap
+    @Test
+    void createTest() {
+        // --Generating values for all different types--
+        int stringSize = rng.nextInt(20);
+        int binarySize = 32;
+
+        // Loop for making a random string of 0s and 1s of length binarySize.
+        StringBuilder generated = new StringBuilder();
+        for (int i = 0; i < binarySize; i++) {
+            if (rng.nextBoolean()) {
+                generated.append("0");
+            } else {
+                generated.append("1");
+            }
+        }
+        generated.toString();
+
+        var a = RandomStringUtils.random(stringSize, 0, Character.MAX_CODE_POINT, true, false, null, rng);;
+        var b = rng.nextBoolean();;
+        var c = rng.nextInt();
+        var d = rng.nextDouble();
+        var e = generated.toString();;
+
+        // --Scrabble Variables with generated values--
+        IScrabbleVariable sa = new ScrabbleString(a);
+        IScrabbleVariable sb = new ScrabbleBool(b);
+        IScrabbleVariable sc = new ScrabbleInt(c);
+        IScrabbleVariable sd = new ScrabbleFloat(d);
+        IScrabbleVariable se = new ScrabbleBinary(e);
+
+        // --Scrabble variable Nodes with generated values--
+        var ta = new NodeString(a);
+        var tb = new NodeBool(b);
+        var tc = new NodeInt(c);
+        var td = new NodeFloat(d);
+        var te = new NodeBinary(e);
+
+        // Different type tests
+        assertEquals(null, VariableFlyweightFactory.getVariable("a"));
+        assertEquals(sa, VariableFlyweightFactory.createScrabbleString("a", a));
+        assertEquals(sa, VariableFlyweightFactory.getVariable("a"));
+        assertEquals(sb, VariableFlyweightFactory.createScrabbleBool("b", b));
+        assertEquals(sb, VariableFlyweightFactory.getVariable("b"));
+        assertEquals(sc, VariableFlyweightFactory.createScrabbleInt("c", c));
+        assertEquals(sc, VariableFlyweightFactory.getVariable("c"));
+        assertEquals(sd, VariableFlyweightFactory.createScrabbleFloat("d", d));
+        assertEquals(sd, VariableFlyweightFactory.getVariable("d"));
+        assertEquals(se, VariableFlyweightFactory.createScrabbleBinary("e", e));
+        assertEquals(se, VariableFlyweightFactory.getVariable("e"));
+    }
+}
